95968e491d06610b3d658430615de2fc0c84ae1c
Your Name <you@example.com>
2022-09-28 10:55:26 -0400

    Mejora

--- pruebaRlg.js
+++ pruebaRlg.js
@@ -1,15 +1,33 @@
 
 var ds = ['L','M','X','J','V','S','D'] //dias de la semana para ingreso de datos
 semana = 3
+diaM=1
+diasem=1
+datos=0
+suma=0
+sumasemana=0
 i = 0
-for (let i=0;i<ds.length-1;i++){
-    var mlluvia = Number(prompt("Ingrese dato publiometrico dia"+ds[i]));//solicitar datos por pantalla
-        mlluvia.push(ds[semana],[semana],)
-        semana++
-        if (semana = 4)  {
-            break;
+for (let i=0;i<=30-1;++i){
+    var mlluvia = Number(prompt("Ingrese dato publiometrico dia"+${i}));//solicitar datos por pantalla
+    datos+=mlluvia    
+    mlluvia.push([semana,diaM,mlluvia],)
+        ++semana
+        ++diaM
+        if (semana == 7)  {
+            suma=true
+            semana=0
+            if((sumasemana/7)>0){
+                console.log("promedio semanal ->$
+                {diasem} es ${sumasemana/7}")
+            }
+sumasemana=0
+diasem++
+}else{
+    if(inisuma){
+        sumasemana+=mlluvia
+    }
         }
 }
-
-console.log("promedio semanal ->",mlluvia/7)
-console.log("promedio mensual",mlluvia/31)    
+console.log(mlluvia)
+console.log(suma)
+console.log("promedio mensual",suma/30)
